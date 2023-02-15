87e83b541a30035ebc1f7a2d3a3a2800be67ae7d
Cameron Morgan <cameron.morgan@skiff.com>
2023-02-15 13:59:23 -0800

    Completed Object 2 Challenge tasks.

---
+++ challenge.js
@@ -0,0 +1,45 @@
+/*
+
+  For this challenge you will be asked to follow the requests below and decipher which selector method you should use.  
+  
+  Once you have finished, you can check your work here:  https://codepen.io/lambdaschool/pen/OoYqNM?editors=1010
+  
+*/ 
+
+/*
+  Request 1: Log the result of every `a` element on the page. The result should be an `HTMLCollection`
+*/
+
+const anchorTags = document.getElementsByTagName("a");
+console.log(anchorTags);
+
+/* 
+  Request 2: Log the result of every `a` element on the page. The result should be a `NodeList`
+*/
+
+const moreAnchorTags = document.querySelectorAll("a");
+console.log(moreAnchorTags);
+
+/* 
+  Request 3: Locate the ID in the HTML. Log the element that the ID is on.
+*/
+
+const customID = document.querySelector("#first-heading");
+console.log(customID);
+
+/* 
+  Request 4: Log the first class that matches ".main-content"
+*/
+
+const mainContent = document.querySelector(".main-content");
+console.log(mainContent);
+
+/* 
+  Request 5: Log the third element with a class of ".nav-item"
+*/
+
+const thirdNavItem = document.querySelector(".nav-item:nth-of-type(3)");
+console.log(thirdNavItem);
+
+
+
