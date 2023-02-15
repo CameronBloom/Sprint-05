12555fdec1e96372ee10501a6e73b785201a50bc
Cameron Morgan <cameron.morgan@skiff.com>
2023-02-15 14:51:17 -0800

    Completed Objective 3 Challenge tasks.

--- Objective03/index.js
+++ Objective03/index.js
@@ -1,6 +1,29 @@
 // Follow the instructions provided in training kit and then follow along here.
 
 const secondHeading = document.querySelector('.second-heading');
-console.log(secondHeading.textContent);
 secondHeading.textContent = "Updated Content";
-console.log(secondHeading.textContent);
+
+const mainHeader = document.querySelector(".main-header").style.color = "green";
+console.log(mainHeader);
+
+const customImg = document.querySelector('.custom-img');
+customImg.src = 'https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350';
+
+const someClass = document.querySelectorAll("p");
+someClass.forEach(elem => {
+  elem.style.color = "white"
+  elem.style.backgroundColor = "pink"
+  elem.style.border = "1px solid black";
+  elem.style.padding = "4px";
+  elem.style.textAlign = "center";
+
+});
+
+const firstLink = document.querySelector("a");
+firstLink.innerText = "Switch!"
+
+const myNav = document.querySelector("nav");
+myNav.classList.add("flex-row")
+
+const allLinks = document.querySelectorAll("a");
+allLinks.forEach(link => link.href="https://www.google.com");
