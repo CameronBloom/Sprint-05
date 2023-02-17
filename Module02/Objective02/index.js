03cdf35a6d51f75d76abd7414ec1df4df178d1b5
Cameron Morgan <cameron.morgan@skiff.com>
2023-02-17 11:54:18 -0800

    Restructured folder and completed Module 02 Objective 02 tasks.

---
+++ Module02/Objective02/index.js
@@ -0,0 +1,54 @@
+/* Use this sample as a quide */
+let sampleButton = document.querySelector('.sampleButton');
+sampleButton.addEventListener('click', (event) => {
+  alert('You Clicked the Sample Button!')
+})
+
+/* NOTE: DO NOT change any HTML or CSS in this exercise, refer to the HTML for the class names of the elements */
+
+/* Step One: Select the container and button elements */
+const docContainer = document.querySelectorAll(".container");
+const docContainerButton = document.querySelectorAll(".button");
+
+/* Step Two: Use the method addEventListener on the button element, listening for a 'click' event */
+/* Step Three: Using the 'event' object in the callback, Change the background color of the button */
+/* Step Four: Click on the button, does it work? */
+docContainerButton[0].addEventListener("click", (event) => { 
+  event.target.style.color = "white";
+  event.target.style.backgroundColor = "green";
+  event.target.style.borderColor = "red";
+  // console.log(event.currentTarget.nodeName);
+});
+
+
+
+/* Step Five: Use the method addEventListener on the container element, listening for a 'mouseenter' event */
+/* Step Six: Using the 'event' object in the callback, Change the background color and the border color of the container */
+docContainer[0].addEventListener("mouseenter", (event) => {
+  event.target.style.backgroundColor = "gray";
+  event.target.style.borderColor = "pink";
+  // console.log(event.currentTarget.nodeName);
+})
+
+/* Step Seven: Reverse the color changes to the container on a 'mouseleave' event */
+docContainer[0].addEventListener("mouseleave", (event) => {
+  event.target.style = "";
+  event.target.style = "";
+  // console.log(event.currentTarget.nodeName);
+})
+
+
+/* Step Eight: Finish the section in TK on stopPropagation */
+
+
+
+/* Step Nine: Create a click event on the container, it can do whatever you want it to, but make it visible */
+
+
+
+/* Step Ten: Click the button, notice how the container click event also triggers? */
+
+/* Step Eleven: Add stopPropagation to the button click handler */
+
+/* Step Twelve: Click the button now, the click should not propagate to the conainer. */
+/* All Done! */
