ed8749cec336e892d387559f6d867e3d041e3ef9
Cameron Morgan <cameron.morgan@skiff.com>
2023-02-17 12:53:17 -0800

    Completed Module 02 Objective 03 tasks.

--- Module02/Objective02/index.js
+++ Module02/Objective02/index.js
@@ -7,16 +7,17 @@ sampleButton.addEventListener('click', (event) => {
 /* NOTE: DO NOT change any HTML or CSS in this exercise, refer to the HTML for the class names of the elements */
 
 /* Step One: Select the container and button elements */
-const docContainer = document.querySelectorAll(".container");
-const docContainerButton = document.querySelectorAll(".button");
+const docContainer = document.querySelector(".container");
+const docContainerButton = document.querySelector(".button");
 
 /* Step Two: Use the method addEventListener on the button element, listening for a 'click' event */
 /* Step Three: Using the 'event' object in the callback, Change the background color of the button */
 /* Step Four: Click on the button, does it work? */
-docContainerButton[0].addEventListener("click", (event) => { 
+docContainerButton.addEventListener("click", (event) => { 
   event.target.style.color = "white";
   event.target.style.backgroundColor = "green";
   event.target.style.borderColor = "red";
+  event.stopPropagation();
   // console.log(event.currentTarget.nodeName);
 });
 
@@ -24,31 +25,26 @@ docContainerButton[0].addEventListener("click", (event) => {
 
 /* Step Five: Use the method addEventListener on the container element, listening for a 'mouseenter' event */
 /* Step Six: Using the 'event' object in the callback, Change the background color and the border color of the container */
-docContainer[0].addEventListener("mouseenter", (event) => {
+docContainer.addEventListener("mouseenter", (event) => {
   event.target.style.backgroundColor = "gray";
   event.target.style.borderColor = "pink";
   // console.log(event.currentTarget.nodeName);
 })
 
 /* Step Seven: Reverse the color changes to the container on a 'mouseleave' event */
-docContainer[0].addEventListener("mouseleave", (event) => {
-  event.target.style = "";
+docContainer.addEventListener("mouseleave", (event) => {
   event.target.style = "";
   // console.log(event.currentTarget.nodeName);
 })
 
-
 /* Step Eight: Finish the section in TK on stopPropagation */
-
-
-
 /* Step Nine: Create a click event on the container, it can do whatever you want it to, but make it visible */
-
-
-
 /* Step Ten: Click the button, notice how the container click event also triggers? */
-
 /* Step Eleven: Add stopPropagation to the button click handler */
-
 /* Step Twelve: Click the button now, the click should not propagate to the conainer. */
+docContainer.addEventListener("click", (event) => {
+  event.target.style.backgroundColor = "orange";
+  event.target.style.borderColor = "black";
+})
+
 /* All Done! */
